0e539bd789b077c97023c352891814aff1e9a258
Vladislav Kuznetsov <super.ewew2015@yandex.ru>
2022-03-09 04:13:27 +0300

    added contract and tests

--- contracts/Donations.sol
+++ contracts/Donations.sol
@@ -3,7 +3,60 @@
 pragma solidity ^0.8.0;
 
 contract Donations {
-  function currentBalance() public view returns(uint) {
+  struct Donation {
+    uint sum;
+    address from;
+  }
+
+  struct Balance {
+    uint totalSum;
+    mapping (uint => Donation) payments;
+  }
+
+  mapping(address => Balance) public balances;
+  address[] addresses;
+  address public owner;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner(){
+    require(msg.sender == owner, "Ownable: caller is not the owner");
+    _;
+  }
+
+  function donat() public payable {
+    uint donatNum = balances[msg.sender].totalSum;
+
+
+    if (balances[msg.sender].totalSum == 0) {
+      addresses.push(msg.sender);
+    }
+
+    balances[msg.sender].totalSum += msg.value;
+
+    Donation memory newDonation = Donation(msg.value, msg.sender);
+
+    balances[msg.sender].payments[donatNum] = newDonation;
+  }
+
+  function withDrawAll(address _addrWalletTo, uint amount) public onlyOwner {
+    require(amount <= address(this).balance, "Insufficient funds");
+    address payable _to = payable(_addrWalletTo);
+
+    _to.transfer(amount);
+  }
+
+  function getTotalBalance() public view returns(uint) {
     return address(this).balance;
+  }
+
+  function getBalanceByAddress(address _addr) public view returns(uint) {
+    return balances[_addr].totalSum;
+  }
+
+  function getClients() public view returns(address[] memory) {
+    return addresses;
   }
 }
--- test/Donations.test.js
+++ test/Donations.test.js
@@ -2,19 +2,58 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 describe('Donations', function () {
-  let donations, acc1, acc2;
+  let donationsContract, ownerAccount, clientAccount, clientAccount2, clientAccount3;
 
   beforeEach(async function() {
-    [acc1, acc2] = await ethers.getSigners();
-    const Donations = await ethers.getContractFactory('Donations', acc1);
+    [ownerAccount, clientAccount, clientAccount2, clientAccount3] = await ethers.getSigners();
 
-    donations = await Donations.deploy();
+    const Donations = await ethers.getContractFactory('Donations', ownerAccount);
 
-    await donations.deployed();
+    donationsContract = await Donations.deploy();
+
+    await donationsContract.deployed();
   });
 
   it('should be deployed', async () => {
     // eslint-disable-next-line
-    expect(donations.address).to.be.properAddress;
+    expect(donationsContract.address).to.be.properAddress;
+  });
+
+  it('balance by default', async () => {
+    const balance = await donationsContract.getTotalBalance();
+    // eslint-disable-next-line
+    expect(balance).to.eq(0);
+  });
+
+  it('sending donations', async () => {
+    const tx = await donationsContract.connect(clientAccount).donat({ value: '100' });
+    const tx2 = await donationsContract.connect(clientAccount2).donat({ value: '25' });
+    const tx3 = await donationsContract.connect(clientAccount3).donat({ value: '10' });
+    const tx4 = await donationsContract.connect(clientAccount2).donat({ value: '15' });
+    const txWithdrawalFunds = await donationsContract.connect(ownerAccount).withDrawAll(clientAccount2.address, 10);
+
+    // eslint-disable-next-line
+    await expect(() => tx).to.changeEtherBalance(clientAccount, -100);
+    // eslint-disable-next-line
+    await expect(() => tx2).to.changeEtherBalance(clientAccount2, -25);
+    // eslint-disable-next-line
+    await expect(() => tx3).to.changeEtherBalance(clientAccount3, -10);
+    // eslint-disable-next-line
+    await expect(() => tx4).to.changeEtherBalance(clientAccount2, -15);
+    // eslint-disable-next-line
+    await expect(() => txWithdrawalFunds).to.changeEtherBalance(clientAccount2, 10);
+    await tx.wait();
+
+    const balanceByAddressClientAccount2 = await donationsContract.getBalanceByAddress(clientAccount2.address);
+    const totalBalance = await donationsContract.getTotalBalance();
+    const clients = await donationsContract.getClients();
+    const numberClients = 3;
+
+    // eslint-disable-next-line
+    expect(totalBalance).to.eq(140);
+    // eslint-disable-next-line
+    expect(balanceByAddressClientAccount2).to.eq(40);
+    // eslint-disable-next-line
+    expect(clients.length).to.eq(numberClients);
   });
 });
